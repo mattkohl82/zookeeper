cc3e37e8598596ec1dac2adee0f35af4900e7c22
mattkohl82 <mattkohl82@gmail.com>
2020-05-28 16:40:00 -0500

    connecting

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express')
 const PORT = process.env.PORT || 3001;
 const app = express()
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -114,6 +115,26 @@ function filterByQuery(query, animalsArray) {
       res.json(animal);
     }
   });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+  
+  
+
+  
+
+  
+
+
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
